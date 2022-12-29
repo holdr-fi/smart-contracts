349f23c099d103b0fbb61b9dbed1c929ea9c6d60
kyzooghost <jeffersonzjtang@gmail.com>
2022-12-29 15:20:09 +1100

    successful fee distribute

--- pkg/deployments/holdr/scripts/vetokens/distributeRewards.ts
+++ pkg/deployments/holdr/scripts/vetokens/distributeRewards.ts
@@ -1,34 +1,32 @@
-// THIS IS FOR TESTNET ONLY
-
-// Deposit 100K SPT tokens into VotingEscrow contract, obtain veSPT
-import { JoinPoolRequest } from '@balancer-labs/balancer-js';
-import { WEEK } from '@balancer-labs/v2-helpers/src/time';
-import { AssetHelpers } from '../../utils';
 import { BigNumber as BN, Contract } from 'ethers';
-import { ZERO, ONE_ETHER, WETH_ADDRESS } from '../../constants';
 import { ContractDeploymentCollection } from '../../types';
 import { ethers, logger, task } from '../../input';
-const { defaultAbiCoder } = ethers.utils;
-const { MaxUint256 } = ethers.constants;
+import BALTokenHolderABI from '../../../tasks/2022xxxx-holdr/abi/BALTokenHolder.json';
 
 export const distributeRewards = async function distributeRewards(
   contractDeploymentCollection: ContractDeploymentCollection
 ): Promise<void> {
   const [deployer] = await ethers.getSigners();
+  const output = task.output({ ensure: false });
   const hldr = contractDeploymentCollection['HoldrGovernanceToken'].instance;
-  const tokenholder = contractDeploymentCollection['BALTokenHolder'].instance;
   const feeDistributor = contractDeploymentCollection['FeeDistributor'].instance;
+  const tokenholder = new Contract(output['BALTokenHolder'], BALTokenHolderABI, deployer.provider);
 
   // 1. Call SingleRecipientGauge.checkpoint(), mint HLDR to Tokenholder
+  logger.info('Calling SingleRecipientGauge.checkpoint()');
   const authorizerAdaptor = contractDeploymentCollection['AuthorizerAdaptor'].instance;
-  await authorizerAdaptor
-    .connect(deployer)
-    .performAction(contractDeploymentCollection['SingleRecipientGauge'].address, '0xc2c4c5c1');
+  await authorizerAdaptor.connect(deployer).performAction(output['SingleRecipientGauge'], '0xc2c4c5c1');
+
+  logger.info('Successful SingleRecipientGauge.checkpoint(), now calling Tokenholder.withdraw()');
 
   // 2. Withdraw HLDR from Tokenholder
   const hldrMinted = await hldr.balanceOf(tokenholder.address);
   await tokenholder.connect(deployer).withdrawFunds(deployer.address, hldrMinted);
 
+  logger.info('Successful Tokenholder.withdraw(), now calling FeeDistrbutor.depositToken()');
+
   // 3. Deposit HLDR into FeeDistributor
   await feeDistributor.connect(deployer).depositToken(hldr.address, hldrMinted);
+
+  logger.info('Successful FeeDistrbutor.depositToken()');
 };
